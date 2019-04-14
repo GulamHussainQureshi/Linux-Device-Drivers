c421540efd3b8008a6a3e3010b971be42a988dc6
GulamHussainQureshi <37244785+GulamHussainQureshi@users.noreply.github.com>
2019-04-14 23:54:35 +0530

    Update i2c_driver.c

--- i2c/i2c_driver.c
+++ i2c/i2c_driver.c
@@ -1,1 +1,13 @@
+#include <linux/version.h>
+#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/mutex.h>
+#include <linux/i2c.h>
+#include <linux/delay.h>
+
